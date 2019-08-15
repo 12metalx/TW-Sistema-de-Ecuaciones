3671ed563657d15cd053f9af4a3aa1a4c196f743
Noe <metal12x@hotmail.com>
2019-08-15 18:20:17 -0500

    Servlet terminado falta diseno

--- src/java/Ecuaciones/solucion.java
+++ src/java/Ecuaciones/solucion.java
@@ -14,22 +14,26 @@ public class solucion extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-       int[][] ma = new int [2][2];
-       int[][] mx = new int[2][2];
-       int[][] my = new int [2][2];
-       ma[0][0] = Integer.parseInt(request.getParameter("x1"));
-       ma[0][1] = Integer.parseInt(request.getParameter("y1"));
-       ma[1][0] = Integer.parseInt(request.getParameter("x2"));
-       ma[1][1] = Integer.parseInt(request.getParameter("y2"));
-       mx[0][0] = Integer.parseInt(request.getParameter("s1"));
-       mx[0][1] = Integer.parseInt(request.getParameter("y1"));
-       mx[1][0] = Integer.parseInt(request.getParameter("s2"));
-       mx[1][1] = Integer.parseInt(request.getParameter("y2"));
-       my[0][0] = Integer.parseInt(request.getParameter("x1"));
-       my[0][1] = Integer.parseInt(request.getParameter("s1"));
-       my[1][0] = Integer.parseInt(request.getParameter("x2"));
-       my[1][1] = Integer.parseInt(request.getParameter("s2"));
-       
+       double[][] ma = new double [2][2];
+       double[][] mx = new double[2][2];
+       double[][] my = new double [2][2];
+       ma[0][0] = Double.parseDouble(request.getParameter("x1"));
+       ma[0][1] = Double.parseDouble(request.getParameter("y1"));
+       ma[1][0] = Double.parseDouble(request.getParameter("x2"));
+       ma[1][1] = Double.parseDouble(request.getParameter("y2"));
+       mx[0][0] = Double.parseDouble(request.getParameter("s1"));
+       mx[0][1] = Double.parseDouble(request.getParameter("y1"));
+       mx[1][0] = Double.parseDouble(request.getParameter("s2"));
+       mx[1][1] = Double.parseDouble(request.getParameter("y2"));
+       my[0][0] = Double.parseDouble(request.getParameter("x1"));
+       my[0][1] = Double.parseDouble(request.getParameter("s1"));
+       my[1][0] = Double.parseDouble(request.getParameter("x2"));
+       my[1][1] = Double.parseDouble(request.getParameter("s2"));
+       double da = determinante(ma);
+       double dx = determinante(mx);
+       double dy = determinante(my);
+       double x = dx/da;
+       double y = dy/da;
         
         try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE html>");
@@ -38,7 +42,13 @@ public class solucion extends HttpServlet {
             out.println("<title>Soluciones</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet solucion at " + request.getContextPath() + "</h1>");
+            if(da!=0){
+             out.println("<h1>La solucion del sitema de ecuaciones es:</h1>");
+            out.print("<p>x = "+x+"<br><br> y = "+y+"</p>");   
+            }
+            else{
+              out.println("<h1>El sistema no tiene solucion.</h1>");  
+            }
             out.println("</body>");
             out.println("</html>");
         }
